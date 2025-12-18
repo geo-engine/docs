52588097c75fb19227f3869e3203300b413d0cc8
ChristianBeilschmidt <ChristianBeilschmidt@users.noreply.github.com>
2025-12-18 18:19:40 +0000

    deploy: c82088a992ebbe7a7610494c891662e6631b36b8

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-9e5a954c.js',
+                path_to_root + 'searchindex-bab93098.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
